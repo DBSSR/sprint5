bba4fc0f40dd56608d2ab0b56021feb418a1f40c
DBSSR <danielabassante@hotmail.com>
2024-06-09 14:05:27 -0500

    upgrade apppy header

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-st.header('Vehicles DataBase')
+st.header('Vehicles Data Base')
 st.write('Seleccione el grafico que desea visualizar')
 
 car_data = pd.read_csv('vehicles_us.csv')
 
 hist_button = st.button('Construir histograma') # crear un botón
-        
+       
 if hist_button: # al hacer clic en el botón
             # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
